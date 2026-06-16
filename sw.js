efbba55ea2ab6f563d33c94d0bd5201d66d4914b
uniboo <e091228@gmail.com>
2026-06-16 23:34:12 +0900

    feat: add PWA support (manifest, SW, washing-machine icon)

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'mente-v1';
+const STATIC = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(fetch(e.request).catch(() => caches.match('/')));
+});
